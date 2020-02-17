f3820d80b2268a7b542122e37753c95e23d6045b
wonjun <dnjswns2280@naver.com>
2020-02-17 09:41:59 +0900

    list, dictionary, for grammar

---
+++ syntax/container.py
@@ -0,0 +1,13 @@
+#string
+print('jun'.capitalize())
+
+#list
+print([1,2,3])
+
+#dictionary
+#값에 대한 이름을 문자로 부여
+d = {'name':'jun',
+    'address':'seoul'}
+
+print(d['name'])
+print(d)
---
+++ syntax/for.py
@@ -0,0 +1,7 @@
+#list
+a = [1,2,3]
+for value in a:
+    print(value)
+
+for value in range(10):
+    print(value)
